cec5f79f61cc4af3f189fc14e2893f84b3ed1638
Dev Gandhi <gandhiDX02@gmail.com>
2024-09-19 09:00:09 -0400

    .gitignore updated

--- main.py
+++ main.py
@@ -4,4 +4,4 @@ from dotenv import load_dotenv
 # Leveraging dotenv to keep the spotify credentials confidential
 load_dotenv()
 client_id = os.getenv('CLIENT_ID')
-client_secret = os.getend('CLIENT_SECRET')
+client_secret = os.getenv('CLIENT_SECRET')
