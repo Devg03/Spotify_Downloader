0a2831361c1c485cf44fe490cc0209309db0b8aa
Dev Gandhi <gandhiDX02@gmail.com>
2024-10-04 14:36:24 -0400

    Spotipy can grab the track name from ID

--- main-spot.py
+++ main-spot.py
@@ -18,6 +18,10 @@ sp = spotipy.Spotify(auth_manager = SpotifyOAuth(client_id = os.getenv('CLIENT_I
                                                  redirect_uri= os.getenv('REDIRECT_URI'),
                                                  scope=scope))
 
-results = sp.search(q='weezer', limit=20)
-for idx, track in enumerate(results['tracks']['items']):
-    print(idx, track['name'])
+# Requesting the track ID
+print("Please enter your track id: ", end = ' ')
+track_id = input()
+
+# Calling Spotify API to receive the track name from its ID
+track_info = sp.track(track_id=track_id, market=None)
+print(track_info['name'])
