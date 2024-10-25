0089d2928a1bac0fd7eb1faaee3842475a22a825
Dev Gandhi <gandhiDX02@gmail.com>
2024-10-25 15:43:53 -0400

    Searchbar + Logs the input url

---
+++ frontend/src/components/search.js
@@ -0,0 +1,22 @@
+import {useState} from "react";
+
+const SearchBar = () => {
+    const [inputValue, setInputValue] = useState('');
+    
+    const handleInputChange = (event) => {
+        setInputValue(event.target.value); // Update state with the input value
+    };
+
+    const handleSubmit = () => {
+        console.log("The input value is:", inputValue); // Do something with the input value
+    };
+    
+    return(
+        <div className="search-container">
+                <input type="text" placeholder="Search.." name="search" onChange={handleInputChange} />
+                <button onClick= {handleSubmit} type="submit">Submit</button> // Submit Button
+        </div> 
+    );
+}
+
+export default SearchBar
