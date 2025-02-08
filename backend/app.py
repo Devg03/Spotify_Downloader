265614031014e252dd951e183dba50fedac5424e
Dev Gandhi <gandhiDX02@gmail.com>
2025-02-08 02:08:54 -0500

    imports added

--- backend/app.py
+++ backend/app.py
@@ -1,18 +1,20 @@
 from flask import Flask, request, jsonify, send_from_directory
-
+import os
+import spotipy
+from dotenv import load_dotenv
+from spotipy.oauth2 import SpotifyOAuth
+from googleapiclient.discovery import build
+    
+load_dotenv()
 app = Flask(__name__, static_folder="../frontend/spoticry/build/", static_url_path="/")
 
 @app.route('/submit', methods=['POST'])
 def submit_data():
     data = request.get_json()
     input_value = data.get('input_data')
+    
 
-    result = {'message': f'Received: {input_value}'}
-    return jsonify(result)
 
 @app.route("/")
-def hello():
-    return send_from_directory(app.static_folder, "index.html") 
-    
-if __name__ == "__main__":
-    app.run()
+def start():
+    return send_from_directory(app.static_folder, "index.html")
