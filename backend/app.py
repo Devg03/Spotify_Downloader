e3cf1b616c1eec1193f5a877bb9dafb865b14af8
Dev Gandhi <gandhiDX02@gmail.com>
2025-02-08 01:16:46 -0500

    Flask is hosting react landing page

--- backend/app.py
+++ backend/app.py
@@ -1,7 +1,10 @@
-from flask import Flask
+from flask import Flask, send_from_directory
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder="../frontend/spoticry/build/", static_url_path="/")
 
 @app.route("/")
 def hello():
-    return "Hello World!"
+    return send_from_directory(app.static_folder, "index.html") 
+    
+if __name__ == "__main__":
+    app.run()
