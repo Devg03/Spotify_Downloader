9fa5a828cd8db7e22cb08f6812a70df56c6a45ca
Dev Gandhi <gandhiDX02@gmail.com>
2025-02-08 00:51:50 -0500

    input can be taken and is stored

--- frontend/spoticry/src/App.js
+++ frontend/spoticry/src/App.js
@@ -1,23 +1,28 @@
+import {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [val, setVal] = useState("");
+  
+  const click = () => {
+    if (val.length > 0) {
+      alert("Submitted!");
+    } else {
+      alert("Please enter a valid track ID.")
+    }
+  }
+
+  const change = event => {
+    const spotiURL = event.target.value;
+    setVal(spotiURL);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className = "App">
+      <h1>Please enter the Spotify track ID: </h1>
+      <input onChange = {change} value = {val} />
+      <button onClick = {click}>Submit!</button>
     </div>
   );
 }
