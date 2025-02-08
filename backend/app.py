c45f52b4f5f9d221201430cddd7be8b97a159040
Dev Gandhi <gandhiDX02@gmail.com>
2025-02-08 01:40:44 -0500

    Flask successfully receives the input

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,14 @@
-from flask import Flask, send_from_directory
+from flask import Flask, request, jsonify, send_from_directory
 
 app = Flask(__name__, static_folder="../frontend/spoticry/build/", static_url_path="/")
+
+@app.route('/submit', methods=['POST'])
+def submit_data():
+    data = request.get_json()
+    input_value = data.get('input_data')
+
+    result = {'message': f'Received: {input_value}'}
+    return jsonify(result)
 
 @app.route("/")
 def hello():
--- frontend/spoticry/src/App.js
+++ frontend/spoticry/src/App.js
@@ -4,25 +4,34 @@ import './App.css';
 
 function App() {
   const [val, setVal] = useState("");
-  
-  const click = () => {
-    if (val.length > 0) {
-      alert("Submitted!");
-    } else {
-      alert("Please enter a valid track ID.")
-    }
-  }
 
   const change = event => {
     const spotiURL = event.target.value;
     setVal(spotiURL);
   }
 
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      const response = await fetch('/submit', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({input_data: val}),
+      });
+      const data = await response.json();
+      console.log(data);
+    } catch (error) {
+      console.error('Error: ', error);
+    }
+  };
+
   return (
     <div className = "App">
       <h1>Please enter the Spotify track ID: </h1>
-      <input onChange = {change} value = {val} />
-      <button onClick = {click}>Submit!</button>
+      <input type = 'text' onChange = {change} value = {val} />
+      <button type = 'submit' onClick = {handleSubmit}>Submit!</button>
     </div>
   );
 }
