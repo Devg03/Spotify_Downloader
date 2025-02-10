c6413ca0c1e618b5f306d819fb69a2b04d43da4b
Dev Gandhi <gandhiDX02@gmail.com>
2025-02-10 14:05:51 -0500

    Flask Reset

--- backend/app.py
+++ backend/app.py
@@ -1,4 +1,5 @@
-from flask import Flask, request, jsonify, send_from_directory
+from flask import Flask, request, jsonify
+from flask_cors import CORS
 import os
 import spotipy
 from dotenv import load_dotenv
@@ -6,15 +7,8 @@ from spotipy.oauth2 import SpotifyOAuth
 from googleapiclient.discovery import build
     
 load_dotenv()
-app = Flask(__name__, static_folder="../frontend/spoticry/build/", static_url_path="/")
+app = Flask(__name__)
+CORS(app, resources={r"/*": {"origins": "*"}}) 
 
-@app.route('/submit', methods=['POST'])
-def submit_data():
-    data = request.get_json()
-    input_value = data.get('input_data')
-    
-
-
-@app.route("/")
-def start():
-    return send_from_directory(app.static_folder, "index.html")
+if __name__ == '__main__':
+    app.run(debug=True)
