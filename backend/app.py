a9981ac1e0d20942cb950e2e3397c04b0ebeb906
Dev Gandhi <gandhiDX02@gmail.com>
2025-02-10 14:29:37 -0500

    FLASK / SPOTIFY API ENV SET

--- backend/app.py
+++ backend/app.py
@@ -1,14 +1,38 @@
-from flask import Flask, request, jsonify
+from flask import Flask, session, request, jsonify
 from flask_cors import CORS
 import os
-import spotipy
 from dotenv import load_dotenv
+from spotipy import Spotify
 from spotipy.oauth2 import SpotifyOAuth
+from spotipy.cache_handler import FlaskSessionCacheHandler
 from googleapiclient.discovery import build
     
 load_dotenv()
 app = Flask(__name__)
+app.config['SECRET_KEY'] = os.urandom(64)
+
 CORS(app, resources={r"/*": {"origins": "*"}}) 
+
+scope = [
+     "user-read-email",
+     "playlist-read-private",
+     "playlist-modify-private",
+     "playlist-modify-public",
+     "playlist-read-collaborative"
+    ]
+
+cache_handler = FlaskSessionCacheHandler(session)
+
+sp_oauth = SpotifyOAuth(
+    client_id = os.getenv('CLIENT_ID'),
+    client_secret = os.getenv('CLIENT_SECRET'),
+    redirect_uri = os.getenv('REDIRECT_URI'),
+    scope = scope,
+    cache_handler = cache_handler,
+    show_dialog = True
+)
+
+sp = Spotify(auth_manager=sp_oauth)
 
 if __name__ == '__main__':
     app.run(debug=True)
